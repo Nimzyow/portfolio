62d39304319346b357154cdc636a5daf28dcee22
Nimzyow <n_soufiani@hotmail.com>
2020-07-31 20:04:58 +0100

    installed react-router-dom and created hello aws

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import { Landing } from "./pages/landing/Landing";
+import { ReactPage } from "./pages/react/ReactPage";
+import { JavaScript } from "./pages/javascript/JavaScript";
+import { Typescript } from "./pages/typescriptPage/typescriptPage";
+import { Node } from "./pages/node/Node";
+import { Redux } from "./pages/redux/redux";
+import { expressPage } from "./pages/express/expressPage";
+import { jestEnzyme } from "./pages/jestEnzyme/jestEnzyme";
+import { MongoDB } from "./pages/mongodb/MongoDB";
+import { reactTestingLib } from "./pages/reactTestingLib/reactTestingLib";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/react" component={ReactPage} />
+        <Route exact path="/javascript" component={JavaScript} />
+        <Route exact path="/typescript" component={Typescript} />
+        <Route exact path="/redux" component={Redux} />
+        <Route exact path="/node" component={Node} />
+        <Route exact path="/express" component={expressPage} />
+        <Route exact path="/jestenzyme" component={jestEnzyme} />
+        <Route exact path="/mongodb" component={MongoDB} />
+        <Route exact path="/reacttestinglib" component={reactTestingLib} />
+      </Switch>
+    </Router>
   );
 }
 
