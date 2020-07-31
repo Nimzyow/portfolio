9e4f71ce2654502c46e30837fc82b41d53d8bc6b
Nimzyow <n_soufiani@hotmail.com>
2020-08-01 00:09:01 +0100

    added first project

--- src/pages/portfolio/Portfolio.js
+++ src/pages/portfolio/Portfolio.js
@@ -19,6 +19,28 @@ export const Portfolio = () => {
         fps={10}
         scale={0.6}
       />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          border: "1px solid black",
+          width: "300px",
+        }}
+      >
+        <h3>Storii</h3>
+        <div
+          style={{
+            display: "flex",
+            width: "100%",
+            border: "1px solid black",
+            justifyContent: "space-evenly",
+          }}
+        >
+          <h4>Github</h4>
+          <h4>Website</h4>
+        </div>
+      </div>
     </div>
   );
 };
