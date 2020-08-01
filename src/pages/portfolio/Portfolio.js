4ee73aa8151757cf91d7cd806947923d06e75f98
Nimzyow <n_soufiani@hotmail.com>
2020-08-01 17:51:53 +0100

    refactored

--- src/pages/portfolio/Portfolio.js
+++ src/pages/portfolio/Portfolio.js
@@ -5,6 +5,39 @@ import { Project } from "../../component/project/Project";
 import frameSheet from "../../assets/frameSheet.png";
 
 export const Portfolio = () => {
+  const projectsGenerator = () => {
+    const projects = [
+      {
+        title: "Storii",
+        github: "https://github.com/ShinyVerse/Storii",
+        website: "https://storii-ui.herokuapp.com/",
+      },
+      {
+        title: "Storii Server",
+        github: "https://github.com/Nimzyow/storii_server",
+        website: "https://storii-server.herokuapp.com/",
+      },
+      {
+        title: "Datespot",
+        github: "https://github.com/Nimzyow/datespot",
+        website: "https://datespot-app.herokuapp.com/",
+      },
+      {
+        title: "DateSpot React-Native",
+        github: "https://github.com/Nimzyow/datespot-reactnative",
+        website: "https://github.com/Nimzyow/datespot-reactnative",
+      },
+    ];
+
+    return projects.map((project) => (
+      <Project
+        title={project.title}
+        github={project.github}
+        website={project.website}
+      />
+    ));
+  };
+
   return (
     <div
       style={{
@@ -28,26 +61,7 @@ export const Portfolio = () => {
           width: "300px",
         }}
       >
-        <Project
-          title={"Storii"}
-          github={"https://github.com/ShinyVerse/Storii"}
-          website={"https://storii-ui.herokuapp.com/"}
-        />
-        <Project
-          title={"Storii Server"}
-          github={"https://github.com/Nimzyow/storii_server"}
-          website={"https://storii-server.herokuapp.com/"}
-        />
-        <Project
-          title={"DateSpot"}
-          github={"https://github.com/Nimzyow/datespot"}
-          website={"https://datespot-app.herokuapp.com/"}
-        />
-        <Project
-          title={"DateSpot React-Native"}
-          github={"https://github.com/Nimzyow/datespot-reactnative"}
-          website={"https://github.com/Nimzyow/datespot-reactnative"}
-        />
+        {projectsGenerator()}
       </div>
     </div>
   );
