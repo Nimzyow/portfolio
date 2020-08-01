c8cdc24c3dc4bfb652392be75bb83d79f3c750ca
Nimzyow <n_soufiani@hotmail.com>
2020-08-01 13:28:18 +0100

    updated portfolio page

--- src/pages/portfolio/Portfolio.js
+++ src/pages/portfolio/Portfolio.js
@@ -31,18 +31,22 @@ export const Portfolio = () => {
         <Project
           title={"Storii"}
           github={"https://github.com/ShinyVerse/Storii"}
+          website={"https://storii-ui.herokuapp.com/"}
         />
         <Project
           title={"Storii Server"}
           github={"https://github.com/Nimzyow/storii_server"}
+          website={"https://storii-server.herokuapp.com/"}
         />
         <Project
           title={"DateSpot"}
           github={"https://github.com/Nimzyow/datespot"}
+          website={"https://datespot-app.herokuapp.com/"}
         />
         <Project
           title={"DateSpot React-Native"}
           github={"https://github.com/Nimzyow/datespot-reactnative"}
+          website={"https://github.com/Nimzyow/datespot-reactnative"}
         />
       </div>
     </div>
