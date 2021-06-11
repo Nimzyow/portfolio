71f6d4f5be26897a63017e0cd3870d531ff34467
Nima Soufiani <>
2021-06-11 08:49:15 +0100

    remove irrelevant files

--- src/App.js
+++ src/App.js
@@ -1,42 +1,24 @@
-import React, { useEffect } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, { useEffect } from "react"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 
-import { Landing } from "./pages/landing/Landing";
-import { Portfolio } from "./pages/portfolio/Portfolio";
+import { Landing } from "./pages/landing/Landing"
+import { Portfolio } from "./pages/portfolio/Portfolio"
 
-import { initGA } from "./index";
-// import { ReactPage } from "./pages/react/ReactPage";
-// import { JavaScript } from "./pages/javascript/JavaScript";
-// import { Typescript } from "./pages/typescriptPage/typescriptPage";
-// import { Node } from "./pages/node/Node";
-// import { Redux } from "./pages/redux/redux";
-// import { expressPage } from "./pages/express/expressPage";
-// import { jestEnzyme } from "./pages/jestEnzyme/jestEnzyme";
-// import { MongoDB } from "./pages/mongodb/MongoDB";
-// import { reactTestingLib } from "./pages/reactTestingLib/reactTestingLib";
+import { initGA } from "./index"
 
 function App() {
-  useEffect(() => {
-    initGA();
-  }, []);
+    useEffect(() => {
+        initGA()
+    }, [])
 
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/portfolio" component={Portfolio} />
-        {/* <Route exact path="/react" component={ReactPage} />
-        <Route exact path="/javascript" component={JavaScript} />
-        <Route exact path="/typescript" component={Typescript} />
-        <Route exact path="/redux" component={Redux} />
-        <Route exact path="/node" component={Node} />
-        <Route exact path="/express" component={expressPage} />
-        <Route exact path="/jestenzyme" component={jestEnzyme} />
-        <Route exact path="/mongodb" component={MongoDB} />
-        <Route exact path="/reacttestinglib" component={reactTestingLib} /> */}
-      </Switch>
-    </Router>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Landing} />
+                <Route exact path="/portfolio" component={Portfolio} />
+            </Switch>
+        </Router>
+    )
 }
 
-export default App;
+export default App
